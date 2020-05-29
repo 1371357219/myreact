a75df37e418e703fb7d0cf3987fded50218e2634
yyr <1371357219@qq.com>
2020-05-29 21:04:36 +0800

    first commit

---
+++ build/precache-manifest.ca7e821c981da69fe6afa8526c2b3708.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40cbd09099ea8e20739a03458210f079",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd7e48c04daa72e9c6ef",
+    "url": "/static/css/main.d8ecfca4.chunk.css"
+  },
+  {
+    "revision": "d4c6a37bb8fdcea52d60",
+    "url": "/static/js/2.262d0629.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.262d0629.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd7e48c04daa72e9c6ef",
+    "url": "/static/js/main.8d499dbf.chunk.js"
+  },
+  {
+    "revision": "233dea908d4de0135036",
+    "url": "/static/js/runtime-main.c8273c4c.js"
+  }
+]);
